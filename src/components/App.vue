a68663672809479f9df8c91cf64fadc508e7f6f2
stanrogo <me@stanrogo.com>
2017-01-22 21:16:24 +0100

    Removed duplicate scss

--- src/components/App.vue
+++ src/components/App.vue
@@ -24,5 +24,110 @@
 <style rel="stylesheet/scss" lang="sass">
     @import '../styles/general';
 
+    // Fonts
 
+    [class*="fontawesome-"]:before {
+        font-family: 'FontAwesome', sans-serif;
+    }
+
+    html {
+        margin: 0;
+        padding: 0;
+        font-size: 16px;
+        color: $color--text-primary;
+
+        * {
+            font-family: 'Cormorant Garamond', serif;
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+        }
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+        min-width: 320px;
+        background-image: url("../assets/crossword.png");
+    }
+
+    h1, h2, h3, h4, h5, h6 {
+        font-weight: 400;
+    }
+
+    ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+
+    img {
+        max-width: 100%;
+    }
+
+    // Helpers
+
+    .material-card {
+        background-color: $color--white;
+        box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    }
+
+    .clearfix{
+        overflow: auto;
+    }
+
+    .text-center{text-align: center;}
+    .text-right{text-align: right;}
+
+    .float-right{float: right};
+    .float-left{float: left};
+
+    .font--white{color: $color--white;}
+    .font--back{color: $color--text-primary;}
+
+    .bg--black{background-color: black;}
+
+    .no-top-margin{margin-top: 0;}
+
+    .hidden{display: none !important;}
+
+    .module{
+        position: relative;
+        display: block;
+        margin-bottom: 2rem;
+    }
+
+    .button{
+        padding: 0.5rem 2rem;
+        background-color: $color--white;
+        color: $color--text-primary;
+        font-size: 1rem;
+        border: 0;
+        box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+        transition: all 0.1s;
+        text-transform: uppercase;
+        cursor: pointer;
+
+        &:hover{
+            box-shadow: 0 3px 5px rgba(0,0,0,0.13), 0 3px 4px rgba(0,0,0,0.22);
+        }
+    }
+
+    .btn:hover{
+        box-shadow: 0 14px 28px rgba(0,0,0,0.13), 0 10px 10px rgba(0,0,0,0.22);
+    }
+
+    .grid-container{
+        position: relative;
+        width: calc(100% - 2rem);
+        max-width: 1040px;
+        margin: 0 1rem;
+        padding: 0 1rem;
+        @include flexbox();
+
+        @include breakpoint(desktop){
+            width: 100%;
+            margin: 0 auto;
+        }
+    }
 </style>

9e867548ea2ddff393f53fb3dd389980ae7ed9fb
stanrogo <me@stanrogo.com>
2017-01-27 22:08:45 +0100

    Created the github module

---
+++ src/components/GitHub.vue
@@ -0,0 +1,68 @@
+<template>
+    <section id="git-hub" class="grid-container material-card">
+        <h1 class="git-title">GitHub</h1>
+        <div class="github-widget" data-username="stanrogo"></div>
+        <div class="git-description">
+            <p>
+                I am currently in the process of becoming more active on GitHub.
+                All my projects that will be worked on during the course of university will be listed here
+                as well as those projects that I personally work on, such as the golangtraining repo which
+                contains my solutions to the project euler exercises in the go language.
+            </p>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'git-hub'
+    }
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+    @import '../styles/general';
+
+    #git-hub{
+        @include flex-direction(row);
+        @include flex-wrap(wrap);
+
+        .git-title{
+            width: 100%;
+        }
+
+        .github-widget{
+            max-width: none;
+            width: 100%;
+
+            @include breakpoint(laptop){
+                width: 50%
+            }
+
+            .gh-widget-container:first-child{
+                display: none;
+            }
+
+            .gh-widget-stats{
+                @include flex-wrap(wrap);
+
+                .gh-widget-item{
+                    @include flex(0 0 50%);
+
+                    @include breakpoint(phablet){
+                        @include flex(1);
+                    }
+                }
+            }
+        }
+
+        .git-description{
+            width: 100%;
+
+            @include breakpoint(laptop){
+                width: 50%;
+                padding: 2rem;
+            }
+        }
+    }
+
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="home">
         <hero></hero>
+        <git-hub></git-hub>
         <skills></skills>
     </div>
 </template>
@@ -8,20 +9,22 @@
 <script>
     import Hero from './Hero.vue';
     import Skills from './Skills.vue';
+    import GitHub from './GitHub.vue';
 
     export default {
         name: 'home',
-        data () {
-            return {
-                msg: 'Welcome to Your Vue.js App'
-            }
-        },
-        components: {Hero, Skills}
+        components: {Hero, GitHub, Skills},
+        created() {
+            this.$nextTick(() => {
+
+                // Re-render the git hub view on route change
+
+                ready(start);
+            });
+        }
     }
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
     @import '../styles/general';
-
-
 </style>

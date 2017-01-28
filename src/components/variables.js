5bc96b54a9d1c226acd92c7162aed7e02ac4bea1
stanrogo <me@stanrogo.com>
2017-01-28 13:13:44 +0100

    Moved cloudinary to use https

--- src/components/variables.js
+++ src/components/variables.js
@@ -1,9 +1,9 @@
 export default {
-    imageBaseURL5: 'http://res.cloudinary.com/stanrogo/image/upload/v1485563095/',
-    imageBaseURL4: 'http://res.cloudinary.com/stanrogo/image/upload/v1485563094/',
-    imageBaseURL6: 'http://res.cloudinary.com/stanrogo/image/upload/v1485563096/',
-    imageBaseURL7: 'http://res.cloudinary.com/stanrogo/image/upload/v1485563097/',
-    imageBaseURL9: 'http://res.cloudinary.com/stanrogo/image/upload/v1485563099/',
-    imageBaseURL: 'http://res.cloudinary.com/stanrogo/image/upload/v148556309',
-    imageBaseURLStripped:'http://res.cloudinary.com/stanrogo/image/upload/'
+    imageBaseURL5: 'https://res.cloudinary.com/stanrogo/image/upload/v1485563095/',
+    imageBaseURL4: 'https://res.cloudinary.com/stanrogo/image/upload/v1485563094/',
+    imageBaseURL6: 'https://res.cloudinary.com/stanrogo/image/upload/v1485563096/',
+    imageBaseURL7: 'https://res.cloudinary.com/stanrogo/image/upload/v1485563097/',
+    imageBaseURL9: 'https://res.cloudinary.com/stanrogo/image/upload/v1485563099/',
+    imageBaseURL: 'https://res.cloudinary.com/stanrogo/image/upload/v148556309',
+    imageBaseURLStripped:'https://res.cloudinary.com/stanrogo/image/upload/'
 }

f9dff57f4f5da2648fdf1ed174b162695ca68757
stanrogo <me@stanrogo.com>
2017-01-29 18:19:15 +0100

    Implemented JS font loader so start render time is faster

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 
+import WebFont from 'webfontloader';
+
 import App from './components/App.vue'
 import Landing from './components/Landing.vue'
 import Home from './components/home/Home.vue'
@@ -44,8 +46,18 @@ router.afterEach(() => {
     window.scrollTo(0, 0);
 });
 
-new Vue({
+const vue = new Vue({
     el: '#app',
     render: h => h(App),
     router
+});
+
+WebFont.load({
+    google: {
+        families: ['Cormorant Garamond']
+    },
+    custom: {
+        families: ['FontAwesome'],
+        urls: ['https://use.fontawesome.com/405bb302d1.css']
+    }
 });

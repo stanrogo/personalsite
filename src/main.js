b3355f2d3cd0b51b4d218971ed054cc166cdb829
stanrogo <me@stanrogo.com>
2017-05-21 12:33:25 +0200

    Enable build file splitting

--- src/main.js
+++ src/main.js
@@ -1,23 +1,55 @@
+// Import Vue dependencies
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 
-import WebFont from 'webfontloader';
+// Import 3rd party dependencies
 
-import App from './components/App.vue'
-import Home from './components/home/Home.vue'
-import Work from './components/work/Work.vue'
-import Blog from './components/blog/Blog.vue'
-import Post from './components/blog/Post.vue'
+import WebFont from 'webfontloader';
+import VueFire from 'vuefire';
+
+// Import the Vuex Store
 
 import store from './vuex/index.js';
 
-const VueFire = require('vuefire');
-const Firebase = require('firebase');
+// Import the main application file
+
+import App from './components/App.vue'
+
+// Import top level route modules as async components (for packaging as separate bundles)
+
+const Home = resolve => {
+    require.ensure(['./components/home/Home.vue'], () => {
+        resolve(require('./components/home/Home.vue'))
+    })
+};
+
+const Work = resolve => {
+    require.ensure(['./components/work/Work.vue'], () => {
+        resolve(require('./components/work/Work.vue'))
+    })
+};
+
+const Blog = resolve => {
+    require.ensure(['./components/blog/Blog.vue'], () => {
+        resolve(require('./components/blog/Blog.vue'))
+    })
+};
+
+const Post = resolve => {
+    require.ensure(['./components/blog/Post.vue'], () => {
+        resolve(require('./components/blog/Post.vue'))
+    })
+};
+
+// Bootstrap our application
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
 Vue.use(VueFire);
+
+// Define the routes we want to take and inject them
 
 const routes = [
     {
@@ -37,7 +69,8 @@ const routes = [
     },
     {
         path: '/blog/:htmlTitle',
-        component: Post
+        component: Post,
+        name: 'Blog'
     }
 ];
 
@@ -61,6 +94,8 @@ const vue = new Vue({
     render: h => h(App),
     router
 });
+
+// Load web fonts in a non-blocking way
 
 WebFont.load({
     google: {

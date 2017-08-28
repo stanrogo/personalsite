0385d8c21ee8e8e7cffb0bf8a888a10eb33f4a94
stanrogo <me@stanrogo.com>
2017-08-28 23:12:26 +0200

    Deleted ignored files

--- storage/framework/views/a1d3c85f061b90b00dcaf917079991993b8536c1.php
+++
@@ -1,6 +0,0 @@
-<?php $__env->startSection('page-template'); ?>
-
-    <?php echo $__env->make('includes.hero', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('default', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>

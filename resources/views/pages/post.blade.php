e19dfc9a64d8659537bfbd35b0c762e301648a54
stanrogo <me@stanrogo.com>
2017-09-02 23:26:21 +0200

    Added comment functionality

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -12,22 +12,49 @@
             <img class="cover" src="{{$cover_image}}">
             <div class="cover--title">
                 <h1><span>{{$post->getTitle()}}</span></h1>
-                <h2><span>By Stanley Clark</span></h2>
+                <h2 class="PostAuthor"><span>By Stanley Clark</span></h2>
             </div>
         </div>
         <a class="button" href="{{route('blog')}}"><span class="lnr lnr-arrow-left ArrowLeft"></span>Back</a>
 
         <div class="post-content grid-container">
 
+            <div class="TagContainer">
+                <span class="lnr lnr-tag"></span>
+                @foreach($post->getTags() as $tag)
+                    <span class="Tag">{{$tag}}</span>
+                @endforeach
+
+                <span class="lnr lnr-clock"></span>
+
+                    <span class="Tag">{{date_format($post->getDate(), "Y/m/d H:i:s")}}</span>
+            </div>
+
             <div>
                 {{\Illuminate\Mail\Markdown::parse($post->getContent())}}
             </div>
+
+            <div id="disqus_thread"></div>
         </div>
     </article>
 @endsection
 
 @section('footer-scripts')
+    <script>
+
+        var disqus_config = function () {
+            this.page.url = 'http://stanrogo.com{{$_SERVER['REQUEST_URI']}}';  // Replace PAGE_URL with your page's canonical URL variable
+            this.page.identifier = '{{$post->getId()}}';
+        };
+        (function() { // DON'T EDIT BELOW THIS LINE
+            var d = document, s = d.createElement('script');
+            s.src = 'https://stanrogo.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+        })();
+    </script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"
             onload="hljs.initHighlighting();"
-    ></script>
+    >
+    </script>
 @endsection

7a9fb9df3b16e059911c17884d5cbd9ed6a941f7
Stanley Clark <me@stanrogo.com>
2018-09-01 21:11:54 +0100

    Updated navbar to use router-link

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,14 +1,13 @@
 <template>
 <b-navbar toggleable="md" type="dark" variant="dark" fixed="top" class="navbar">
     <b-navbar-brand href="/">
-        <img src="/icons/favicon-32x32.png" width="30" height="30"
-			 class="d-inline-block align-top" alt="">
-            Stanrogo
+        <img src="/icons/favicon-32x32.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        Stanrogo
     </b-navbar-brand>
     <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
     <b-collapse is-nav id="nav_collapse">
         <b-navbar-nav class="ml-auto">
-            <b-nav-item :href="'/' + item.to" v-for="item in navItems" :key="item.name" :active="item.active" >
+            <b-nav-item :to="item.to" v-for="item in navItems" :key="item.name" exact>
                 {{item.name}}
             </b-nav-item>
         </b-navbar-nav>
@@ -24,41 +23,18 @@ export default {
             navItems: [
                 {
                     name: 'Home',
-                    to: '',
-                    active: false,
+                    to: '/',
                 },
                 {
                     name: 'Blog',
                     to: 'blog',
-                    active: false,
                 },
                 {
                     name: 'About',
                     to: 'about',
-                    active: false,
                 },
             ],
         };
-    },
-    methods: {
-        inView(elID){
-            const element = document.getElementById(elID);
-            if(!element) return false;
-            const coords = element.getBoundingClientRect();
-            return coords.bottom > 56 && coords.bottom <= coords.height + 56;
-        },
-        handleScroll(){
-            this.navItems.forEach((item) => {
-                item.active = this.inView(item.id);
-            });
-        },
-    },
-    destroyed(){
-        window.removeEventListener('scroll', this.handleScroll);
-    },
-    mounted(){
-		window.addEventListener('scroll', this.handleScroll);
-        this.handleScroll();
     },
 };
 </script>

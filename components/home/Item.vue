e992ec5cada58364dd18ca65068e5f363d9069c0
Stanley Clark <me@stanrogo.com>
2019-06-18 17:34:56 +0200

    Properly size images on homepage using picture

--- components/home/Item.vue
+++ components/home/Item.vue
@@ -1,7 +1,12 @@
 <template>
 	<div class="portfolio-item text-center d-flex flex-column">
 		<router-link :to="'/' + item.type + '/' + item.link + '/'" tag="figure" class="portfolio-item--figure bg-main">
-			<img v-if="item.img" :src="item.img + '?w=350&h=350&fit=crop&fm=jpg&fl=progressive'" class="portfolio-item--image">
+			<picture v-if="item.img">
+				<source :srcset="item.img + '?w=350&h=350&fit=crop&fm=jpg&fl=progressive'" media="(min-width: 1200px)">
+				<source :srcset="item.img + '?w=290&h=290&fit=crop&fm=jpg&fl=progressive'" media="(min-width: 992px)">
+				<source :srcset="item.img + '?w=210&h=210&fit=crop&fm=jpg&fl=progressive'" media="(min-width: 768px)">
+				<img :src="item.img + '?w=240&h=240&fit=crop&fm=jpg&fl=progressive'" class="portfolio-item--image">
+			</picture>
 		</router-link>
 		<div>{{ item.name }}</div>
 		<div class="text-secondary">{{ item.sub }}</div>

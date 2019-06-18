09b8c6c8af1c76e733f405ce946f7a5aab7a6f99
Stanley Clark <me@stanrogo.com>
2019-06-18 16:50:35 +0200

    Preconnect to required origins

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,9 @@ module.exports = {
 		link: [
 			{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico',},
 			{rel: 'manifest', href: '/manifest.json',},
+			{rel: 'preconnect', href: 'https://cdn.linearicons.com',},
+			{rel: 'preconnect', href: 'https://cdn.contentful.com',},
+			{rel: 'preconnect', href: 'https://ajax.googleapis.com',},
 		],
 	},
 	modules: [

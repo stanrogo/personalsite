e16acab3d36baa5d36c27715af48b358acf8da25
Stanley Clark <me@stanrogo.com>
2019-06-18 17:24:31 +0200

    Add rel=noopener to external links

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,6 +1,6 @@
 <template>
 	<section id="contact" class="d-flex align-items-center justify-content-center flex-wrap">
-		<a v-for="link in contactLinks" :key="link.name" :href="link.link" target="_blank"
+		<a v-for="link in contactLinks" :key="link.name" :href="link.link" target="_blank" rel="noopener"
 			class="text-body text-uppercase font-small text-hover-secondary px-3 pb-3">
 			{{ link.name }}
 		</a>
